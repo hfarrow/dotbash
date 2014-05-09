5018816994a7265ebf76d003df2e1ece737ea386
Heath Farrow <heath.farrow@disney.com>
2014-05-09 09:28:38 -0700

    Added option to setup.sh to allow custom config if desired config is not in the selection list.

--- setup.sh
+++ setup.sh
@@ -2,15 +2,28 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-ln -s $DIR/ctags ~/.ctags
-ln -s $DIR/git-completion.sh ~/.git-completion.sh
-ln -s $DIR/git-prompt.sh ~/.git-prompt.sh
+echo "Setting up common configuration."
+files=(ctags git-completion.sh git-prompt.sh)
+for i in "${files[@]}"
+do
+	echo "Creating symbolic link: ~/.${i} -> ${DIR}/${i}"
+    ln -s $DIR/$i ~/.$i
+done
 
+echo ""
 echo "----------------------------------------------------------------------------------------"
 echo "Please select a configuration by entering the configuration number and pressing enter: "
 echo "----------------------------------------------------------------------------------------"
-select CONFIG_NAME in home-mint work-mac
+select CONFIG_NAME in home-mint work-mac other
 do
+    case $CONFIG_NAME in 
+        "other")
+            echo ""
+            read -e -p "Enter configuration name (eg: work-mac): " CONFIG_NAME
+            ;;
+    esac
+
+    echo ""
     echo "Setting up [${CONFIG_NAME}]."
     . $DIR/$CONFIG_NAME/setup.sh
     exit
