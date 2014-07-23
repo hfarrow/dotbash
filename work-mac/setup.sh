a1bdfb56f9546abbf059ba901bf4f1ddf29044f9
Heath Farrow <heath.farrow@disney.com>
2014-07-23 09:56:14 -0700

    Added diffmerge scripts to work-mac

--- work-mac/setup.sh
+++ work-mac/setup.sh
@@ -8,3 +8,10 @@ do
 	echo "Creating symbolic link: ~/.${i} -> ${DIR}/${i}"
     ln -s $DIR/$i ~/.$i
 done
+
+bin_files=(diffmerge svndiffmerge)
+for i in "${bin_files[@]}"
+do
+    echo "Creating symbolic link: /usr/local/bin/${i} -> ${DIR}/${i}"
+    ln -s ${DIR}/$i /usr/local/bin/$i
+done
